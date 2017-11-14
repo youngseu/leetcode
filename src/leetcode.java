a0df85554972333f9846c794e7751d6e518a804b
youngseu <546732304@qq.com>
2017-11-14 22:54:07 +0800

    11.14 21 22

--- src/leetcode.java
+++ src/leetcode.java
@@ -595,8 +595,66 @@ public class leetcode {
         }
     }
 
+    //problem 21
+    //Merge Two Sorted Lists
+    static class Solution21 {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode head = new ListNode(0);
+            ListNode curr = head;
+            while (l1 != null || l2 != null) {
+                if (l1 != null && l2 != null && l1.val < l2.val) {
+                    curr.next = new ListNode(l1.val);
+                    l1 = l1.next;
+                } else if (l1 != null && l2 != null && l1.val > l2.val) {
+                    curr.next = new ListNode(l2.val);
+                    l2 = l2.next;
+                } else if (l1 == null) {
+                    curr.next = new ListNode(l2.val);
+                    l2 = l2.next;
+                } else if (l2 == null) {
+                    curr.next = new ListNode(l1.val);
+                    l1 = l1.next;
+                }
+                curr = curr.next;
+            }
+            return head.next;
+        }
+
+        //using recursion
+        public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+
+    //problem 22
+    //Generate Parentheses
+    static class Solution22 {
+        public List<String> generateParenthesis(int n) {
+            List<String> res = new ArrayList<String>();
+            generate(res, "", n, n);
+            return res;
+        }
+
+        public void generate(List<String> res, String s, int left, int right) {
+            if (left == 0 && right == 0)
+                res.add(s);
+            if (left > 0)
+                generate(res, s + "(", left - 1, right);
+            if (left < right)
+                generate(res, s + ")", left, right - 1);
+        }
+    }
+
     public static void main(String[] args) {
-        Solution20 s = new Solution20();
-        System.out.println(s.isValid("([])"));
+        Solution22 s = new Solution22();
+        System.out.println(s.generateParenthesis(4));
     }
 }
