51d5c23fb8eec1a54c16809cd86e3ab03fd8351d
youngseu <546732304@qq.com>
2017-12-19 15:51:24 +0800

    12.19 24

--- src/leetcode.java
+++ src/leetcode.java
@@ -653,6 +653,91 @@ public class leetcode {
         }
     }
 
+    //problem 23
+    //Merge k Sorted Lists
+    static class Solution23 {
+        //Merge with Divide And Conquer
+        public ListNode mergeKLists(ListNode[] lists) {
+            return mergeKLists(lists, 0, lists.length - 1);
+        }
+
+        public ListNode mergeKLists(ListNode[] lists, int start, int end) {
+            if (start == end) return lists[start];
+            else if (start < end) {
+                int mid = (start + end) / 2;
+                ListNode l1 = mergeKLists(lists, start, mid);
+                ListNode l2 = mergeKLists(lists, mid + 1, end);
+                return merge2Lists(l1, l2);
+            } else return null;
+        }
+
+        public ListNode merge2Lists(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val < l2.val) {
+                l1.next = merge2Lists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = merge2Lists(l1, l2.next);
+                return l2;
+            }
+        }
+
+        //Optimize Approach 2 by Priority Queue
+        //priority queue to find the small num
+        public ListNode mergeKLists2(ListNode[] lists) {
+            if (lists == null || lists.length == 0) return null;
+            PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.length,
+                    new Comparator<ListNode>() {
+                        @Override
+                        public int compare(ListNode o1, ListNode o2) {
+                            return o1.val - o2.val;
+                        }
+                    });
+            ListNode dummy = new ListNode(0);
+            ListNode tail = dummy;
+            for (ListNode node : lists)
+                if (node != null)
+                    queue.add(node);
+            while (!queue.isEmpty()) {
+                tail.next = queue.poll();
+                tail = tail.next;
+                if (tail.next != null)
+                    queue.add(tail.next);
+            }
+            return dummy.next;
+        }
+
+        //problem 24
+        //Swap Nodes in Pairs
+        static class Solution24 {
+            //recursion
+            public ListNode swapPairs(ListNode head) {
+                if (head == null || head.next == null) return head;
+                ListNode n = head.next;
+                head.next = swapPairs(head.next.next);
+                n.next = head;
+                return n;
+            }
+
+            //iteration
+            public ListNode swapPairs1(ListNode head) {
+                ListNode dummy = new ListNode(0);
+                dummy.next = head;
+                ListNode current = dummy;
+                while (current.next != null && current.next.next != null) {
+                    ListNode first = current.next;
+                    ListNode second = current.next.next;
+                    first.next = second.next;
+                    current.next = second;
+                    current.next.next = first;
+                    current = current.next.next;
+                }
+                return dummy.next;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Solution22 s = new Solution22();
         System.out.println(s.generateParenthesis(4));
