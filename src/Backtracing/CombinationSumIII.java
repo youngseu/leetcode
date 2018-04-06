392d7485c0818e69762ecd9193ced61e017a5e11
youngseu <546732304@qq.com>
2018-04-06 23:06:01 +0800

    2018.4.6

---
+++ src/Backtracing/CombinationSumIII.java
@@ -0,0 +1,19 @@
+package Backtracing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//216 problem
+//Combination Sum III
+//Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9
+//can be used and each combination should be a unique set of numbers.
+//Example 2:
+//Input: k = 3, n = 9
+//Output:
+//[[1,2,6], [1,3,5], [2,3,4]]
+public class CombinationSumIII {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        return res;
+    }
+}
---
+++ src/DynamicProgramming/WildcardMatching.java
@@ -0,0 +1,65 @@
+package DynamicProgramming;
+
+import com.sun.org.apache.bcel.internal.generic.RET;
+
+//44 problem
+//Wildcard Matching
+//'?' Matches any single character.
+//'*' Matches any sequence of characters (including the empty sequence).
+//
+//The matching should cover the entire input string (not partial).
+//
+//The function prototype should be:
+//bool isMatch(const char *s, const char *p)
+//
+//Some examples:
+//isMatch("aa","a") → false
+//isMatch("aa","aa") → true
+//isMatch("aaa","aa") → false
+//isMatch("aa", "*") → true
+//isMatch("aa", "a*") → true
+//isMatch("ab", "?*") → true
+//isMatch("aab", "c*a*b") → false
+public class WildcardMatching {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = false;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) != '*') {
+                    dp[i][j] = dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?');
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static boolean f(String s, int l, String p, int r) {
+        if (p == "*") return true;
+        int i = 0, j = 0;
+        while (i != s.length() && j != p.length()) {
+            if (p.charAt(j) == '?') {
+                i++;
+                j++;
+            } else if (p.charAt(j) == '*') {
+
+            } else if (s.charAt(i) != p.charAt(j))
+                return false;
+        }
+        return false;
+    }
+
+}
